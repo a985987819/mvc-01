6423a54378ec47432bcb86b08b666155314ca4f8
laoduan <985987819@qq.com>
2020-10-10 15:42:01 +0800

    mvc模式v1

---
+++ src/app4.js
@@ -0,0 +1,16 @@
+import $ from "jQuery"
+import './app4.css'
+const html = `
+  <section id="app4">
+    <div class="circle"></div>
+  </section>
+`
+const $element = $(html).appendTo($('body>.page'))
+const $circle = $('#app4 .circle')
+
+
+$circle.on('mouseenter',()=>{
+  $circle.addClass('active')
+}).on('mouseleave',()=>{
+  $circle.removeClass('active')
+})
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import x from './app1.js'
+import y from './app2.js'
+import './app3.js'
+import './app4.js'
+
+import './reset.css'
+import './global.css'
+
+x.init('#app1')
+y.init('#app2')
+
+
